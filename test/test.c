79b35636a488fdf324a2ba1c95fda49c32df2cf5
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-06-02 23:04:55 +0200

    [UP] Update test.

--- test/test.c
+++ test/test.c
@@ -12,6 +12,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#define MAX_PLAYER 4
+
 typedef struct Player_s
 {
 
@@ -20,15 +22,30 @@ typedef struct Player_s
 
 } Player;
 
+typedef struct World_s {
+
+    Player *players[MAX_PLAYER];
+    size_t  nplayers;
+
+} World;
+
 int onConnectionCallback([[maybe_unused]] knServer *server, knConnection *conn)
 {
-    Player *player = malloc(sizeof(Player));
+    World  *w = knServer_getData(server);
+    Player *player;
 
-    if (!player)
+    if (!w || w->nplayers >= MAX_PLAYER) {
         return -1;
+    }
+    player = malloc(sizeof(Player));
+    if (!player) {
+        return -1;
+    }
     player->conn = conn;
     player->x = rand() % 255;
     knConnection_setData(conn, player);
+    w->players[w->nplayers] = player;
+    w->nplayers++;
     printf("[%d] Hello guys from callback!!\n", player->x);
     return 0;
 }
@@ -46,10 +63,18 @@ int onReadCallback([[maybe_unused]] knConnection *conn, const char *str, size_t 
 int onDisconnectionCallback([[maybe_unused]] knServer *server, knConnection *conn)
 {
     Player *player = knConnection_getData(conn);
+    World  *w      = knServer_getData(server);
 
-    if (!player)
+    if (!player || !w)
         return -1;
     printf("[%d] Guys i'm tired\n", player->x);
+    for (size_t i = 0; i < w->nplayers; ++i) {
+        if (w->players[i] == player) {
+            w->players[i] = w->players[w->nplayers - 1];
+            w->players[w->nplayers - 1] = NULL;
+            w->nplayers--;
+        }
+    }
     free(player);
     return 0;
 }
@@ -57,6 +82,9 @@ int onDisconnectionCallback([[maybe_unused]] knServer *server, knConnection *con
 int main(void)
 {
     knServer *server = knServer_create(4242);
+    World     world = {{}, 0};
+
+    knServer_setData(server, &world);
 
     knServer_setLogging(server, true);
     knCallback_onConnection(server, &onConnectionCallback);
@@ -67,6 +95,9 @@ int main(void)
     while (knServer_isRunning(server)) {
         knServer_runOnce(server, 2000);
         printf("Update game loop\n");
+        for (size_t i = 0; i < world.nplayers; ++i) {
+            printf("Player with fd [%d] is alive!\n", world.players[i]->conn->fd);
+        }
     }
     knServer_destroy(server);
     return 0;
