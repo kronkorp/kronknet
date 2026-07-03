e7c2b71c5486c784a87edcb440204f117fda3828
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-07-03 13:28:03 +0200

    [ADD] Add tests

--- tests/utils/tests_knmap.c
+++ tests/utils/tests_knmap.c
@@ -1,6 +1,8 @@
-#include <criterion/criterion.h>
+#include <stddef.h>
+#include <kronknet/utils/hashmap/hashmap.h>
+#include <stdint.h>
 #include <string.h>
-#include "kronknet/utils/hashmap/hashmap.h"
+#include <heracles/heracles.h>
 
 static uint64_t dummy_collision_hash(uint64_t key) {
     (void)key;
@@ -25,17 +27,17 @@ static void spy_deleter(void *data) {
 Test(hashmap, insert_and_search_nominal)
 {
     knMap *map = knMap_create(stable_hash, 8);
-    cr_assert_not_null(map);
-    
+    AssertExists(map, "Map is null");
+
     char *val1 = strdup("Client_UDP_1");
     char *val2 = strdup("Client_UDP_2");
 
-    cr_assert_eq(knMap_insert(map, 111122223333ULL, val1, free), 0);
-    cr_assert_eq(knMap_insert(map, 444455556666ULL, val2, free), 0);
+    AssertEq(knMap_insert(map, 111122223333ULL, val1, free), 0, "not equal");
+    AssertEq(knMap_insert(map, 444455556666ULL, val2, free), 0, "not equal");
 
-    cr_assert_eq(knMap_search(map, 111122223333ULL), val1);
-    cr_assert_eq(knMap_search(map, 444455556666ULL), val2);
-    cr_assert_null(knMap_search(map, 999999999999ULL));
+    AssertEq(knMap_search(map, 111122223333ULL), val1, "not equal");
+    AssertEq(knMap_search(map, 444455556666ULL), val2, "not equal");
+    Assert(knMap_search(map, 999999999999ULL) == NULL, "not equal");
 
     knMap_destroy(map);
 }
@@ -43,18 +45,18 @@ Test(hashmap, insert_and_search_nominal)
 Test(hashmap, overwrite_and_deleter)
 {
     knMap *map = knMap_create(stable_hash, 8);
-    cr_assert_not_null(map);
+    AssertExists(map, "not equal");
     g_delete_calls = 0;
 
     char *val1 = strdup("Premier_Passage");
     char *val2 = strdup("Deuxieme_Passage");
 
     knMap_insert(map, 42ULL, val1, spy_deleter);
-    cr_assert_eq(g_delete_calls, 0);
+    AssertEq(g_delete_calls, 0, "not equal");
 
     knMap_insert(map, 42ULL, val2, spy_deleter);
-    cr_assert_eq(g_delete_calls, 1);
-    cr_assert_eq(knMap_search(map, 42ULL), val2);
+    AssertEq(g_delete_calls, 1, "not equal");
+    AssertEq(knMap_search(map, 42ULL), val2, "not equal");
 
     knMap_destroy(map);
 }
@@ -62,7 +64,7 @@ Test(hashmap, overwrite_and_deleter)
 Test(hashmap, hash_collision_handling)
 {
     knMap *map = knMap_create(dummy_collision_hash, 8);
-    cr_assert_not_null(map);
+    AssertExists(map, "Map is null");
 
     char *val1 = strdup("Data_1");
     char *val2 = strdup("Data_2");
@@ -70,8 +72,8 @@ Test(hashmap, hash_collision_handling)
     knMap_insert(map, 10ULL, val1, free);
     knMap_insert(map, 20ULL, val2, free);
     
-    cr_assert_eq(knMap_search(map, 10ULL), val1);
-    cr_assert_eq(knMap_search(map, 20ULL), val2);
+    AssertEq(knMap_search(map, 10ULL), val1, "not equal");
+    AssertEq(knMap_search(map, 20ULL), val2, "not equal");
 
     knMap_destroy(map);
 }
@@ -79,17 +81,17 @@ Test(hashmap, hash_collision_handling)
 Test(hashmap, auto_rehash)
 {
     knMap *map = knMap_create(stable_hash, 4);
-    cr_assert_not_null(map);
+    AssertExists(map, "Map is null");
 
     knMap_insert(map, 1ULL, strdup("A"), free);
     knMap_insert(map, 2ULL, strdup("B"), free);
     knMap_insert(map, 3ULL, strdup("C"), free);
     knMap_insert(map, 4ULL, strdup("D"), free);
 
-    cr_assert_not_null(knMap_search(map, 1ULL));
-    cr_assert_not_null(knMap_search(map, 2ULL));
-    cr_assert_not_null(knMap_search(map, 3ULL));
-    cr_assert_not_null(knMap_search(map, 4ULL));
+    AssertExists(knMap_search(map, 1ULL), "Should exists");
+    AssertExists(knMap_search(map, 2ULL), "Should exists");
+    AssertExists(knMap_search(map, 3ULL), "Should exists");
+    AssertExists(knMap_search(map, 4ULL), "Should exists");
 
     knMap_destroy(map);
 }
